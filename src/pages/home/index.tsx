9108a2cb09f97295db9de96832319e65923805c1
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-17 21:05:22 +0900

    feat : todo 조회 및 삭제 기능 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,34 @@
+import { useEffect, useState } from 'react';
+import { deleteTodo, getTodo } from '../../libs/axios/AxiosTodo';
+import { TodoList } from '../../types/todo';
+
 const Home = () => {
-  return <div></div>;
+  const [todoData, setTodoData] = useState<TodoList[]>([]);
+
+  const getTodoList = async () => {
+    const response = await getTodo({ number: 1 });
+    setTodoData(response);
+  };
+  useEffect(() => {
+    getTodoList();
+  }, []);
+
+  const deleteData = async (idx: string) => {
+    await deleteTodo({ todoId: idx });
+    setTodoData(todoData.filter((todo: TodoList) => todo.idx !== idx));
+  };
+
+  return (
+    <div>
+      {todoData.map((todo: TodoList) => (
+        <div key={todo.idx}>
+          <h3>{todo.title}</h3>
+          <p>{todo.content}</p>
+          <button onClick={() => deleteData(todo.idx)}>delete</button>
+        </div>
+      ))}
+    </div>
+  );
 };
+
 export default Home;
