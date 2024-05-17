c742fcaee430b2970f08ed1d8a7cf4cd41f4deb9
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-17 22:28:28 +0900

    fix : signIn 수정했습니다

--- src/pages/sign/sign-in/index.tsx
+++ src/pages/sign/sign-in/index.tsx
@@ -1,24 +1,19 @@
-import { useState } from 'react';
 import { InputForm } from '../../../common/input/input-form';
 import { postSignIn } from '../../../libs/axios/AxiosUser';
-import { user } from '../../../types/api/apiUser';
 
 const SignIn = () => {
-  const [userData, setUserData] = useState<user>({ email: '', pw: '' });
   const onSignUp: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     const form = e.currentTarget;
     const $email = form.elements.namedItem('email') as HTMLFormElement | null;
     const $pw = form.elements.namedItem('pw') as HTMLFormElement | null;
     if (!$email || !$pw) return;
-
     const newData = {
       email: $email.value,
       pw: $pw.value,
     };
-    setUserData(newData);
 
-    await postSignIn(userData);
+    await postSignIn(newData);
   };
 
   return (
