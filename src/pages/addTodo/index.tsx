7bd22ae599cb7c82c9f27039d2f4deda647d46bc
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-17 21:08:08 +0900

    feat : todo 추가 기능넣었습니다

--- src/pages/addTodo/index.tsx
+++ src/pages/addTodo/index.tsx
@@ -1,9 +1,26 @@
 import { InputForm } from '../../common/input/input-form';
+// import { postTodo } from '../../libs/axios/AxiosTodo';
+
+import { postTodo } from '../../libs/axios/AxiosTodo';
 
 const AddTodo = () => {
+  const onSumit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const $title = form.elements.namedItem('title') as HTMLFormElement | null;
+    const $content = form.elements.namedItem('content') as HTMLFormElement | null;
+    if (!$title || !$content) return;
+    const newData = {
+      title: $title.value,
+      content: $content.value,
+    };
+
+    postTodo(newData);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onSumit}>
         <InputForm text="제 목" type="text" id="title" name="title" />
         <InputForm text="할 일" type="text" id="content" name="content" />
         <button type="submit">추가하기 </button>
