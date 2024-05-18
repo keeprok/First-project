e9299ad6f23c5577a4c4f06a1927dad56c9ada3e
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-18 17:05:01 +0900

    refactor : 불필요 코드 제거

--- src/common/button/index.tsx
+++ src/common/button/index.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
-import { cn } from '../../libs/tailwind-css/utils';
+
 import type { ButtonVoid } from '../../types/common';
 import { ButtonVariants } from '../../types/variants';
 
 const Button: FC<ButtonVoid> = ({ shape, color, children, ...props }) => {
   return (
-    <button className={cn(ButtonVariants({ shape, color }))} {...props}>
+    <button className={ButtonVariants({ shape, color })} {...props}>
       {children}
     </button>
   );
